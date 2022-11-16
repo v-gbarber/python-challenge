d4bbb2f5d379a1894e5729a6182b8495b1ea05d1
Valerie Lynn Grannemann-Barber <116371083+v-gbarber@users.noreply.github.com>
2022-11-16 07:58:30 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,58 +1,61 @@
 import os
 import csv
-#file path
-budget_data = os.path.join("resources", "budget_data.csv")
+#output file
+election_data= os.path.join('resources', 'election_data.csv')
+print(os.getcwd())
 
 #Variables
-month=0
-total=0
-date=[]
-change_average=[]
-profits_losses=[]
-profit_loss_change=[]
+votes_cast=0
+county =[]
+list_candidates=[]
+amount_won={}
+percentage_won=0
+winning_candidate=[]
+winning_votes=0
 
-with open (budget_data) as csvfile:
-     Budget = csv.reader(csvfile)
-     header = next(Budget)
-     print(header)
+#open csv
+with open(election_data) as csvfile:
+    csv_reader=csv.reader(csvfile, delimiter= ",")
+    header = next(csvfile)
+    print(f'Header: {header}')
 
-     for row in Budget:
-          month = month + 1
-          total = total + int(row[1])
-          profits_losses.append(row[1])
-          date.append(row[0])
-          print(profits_losses)
-          print(f"month:{month}")
-          print(f"Total: {total}")
+    for row in csv_reader:
+        votes_cast += 1
 
-     for x in range(1,len(profits_losses)):
-         profit_loss_change.append(int(profits_losses[x]) -int(profits_losses[x-1]))
+        if row[2] not in list_candidates:
+            list_candidates.append(row[2])
+            amount_won[row[2]]=0
+            amount_won[row[2]]+=1  
+        else: 
+            amount_won[row[2]]+=1 
+print(amount_won)
+for candidate in amount_won:
+    votes=amount_won.get(candidate)
+    vote_percentage=votes / votes_cast * 100
+    print(f'{candidate}: {votes} {vote_percentage}%')
+    if votes > winning_votes:
+        winning_votes=votes
+        winning_candidate = candidate
 
-     change_average = sum(profit_loss_change) / len(profit_loss_change)
-     profit_increase = max(profit_loss_change)
-     profit_decrease = min(profit_loss_change)
-     profit_increase_date = date[profit_loss_change.index(profit_increase)+1]
-     profit_decrease_date = date[profit_loss_change.index(profit_decrease)+1]
-     print(f"Greatest Increase of Profits: {profit_increase_date} (${profit_increase_date})")
-     print(f"Greatest Decrease of Profits: {profit_decrease_date} (${profit_decrease_date})")
-    
-#Summary
 
-print(f'summary')
-print(f'Financial Analysis')
-print(f'----------------------')
-print(f'Total Months : {month}')
-print(f'Total: ${total}')
-print(f'Average Change: ${change_average}')
-print(f'Greatest Increase of Profits: {profit_increase_date} (${profit_increase}')
-print(f'Greatest Decrease of Profits:{profit_decrease_date} (${profit_decrease}')
-path_to_file = os.path.join("Analysis", "Pybank_code.txt")
-with open (path_to_file, "w") as txtfile:
-     txtfile.write(f'summary\n')
-     txtfile.write(f'Financial Analysis\n')
-     txtfile.write(f'----------------------\n')
-     txtfile.write(f'Total Months : {month}\n')
-     txtfile.write(f'Total: ${total}\n')
-     txtfile.write(f'Average Change: ${change_average}\n')
-     txtfile.write(f'Greatest Increase of Profits: {profit_increase_date} (${profit_increase}\n')
-     txtfile.write(f'Greatest Decrease of Profits:{profit_decrease_date} (${profit_decrease}\n')
+
+#Percentage 
+#Pypoll_Analysis
+
+print(f"Election Results")
+print(f"------------------------")
+print(f"Total Votes: {votes_cast}")
+print(f"------------------------")
+print(f'Winner: {winning_candidate}')
+print(winning_votes)
+file_to_save = os.path.join("analysis", "Pypoll_code.txt")
+with open (file_to_save, "w") as txtfile:
+    txtfile.write(f"Election Results\n")
+    txtfile.write(f"----------------------\n")
+    txtfile.write(f"Total Votes: {votes_cast}\n")
+    txtfile.write(f"----------------------\n")
+    txtfile.write(f'Winner: {winning_candidate}\n')
+    txtfile.write(f'{candidate}: {votes} {vote_percentage}% \n')
+    txtfile.write(f'{amount_won}\n')
+    txtfile.write(f'{winning_votes}\n')
+
